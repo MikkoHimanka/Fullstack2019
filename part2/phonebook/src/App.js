3f0bf33ab219ebb19b0552cfe008e3019e25c3c1
Mikko Himanka <mikko.himanka@protonmail.com>
2019-10-31 14:39:56 +0200

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,11 +6,14 @@ const Person = (props) => {
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -18,6 +21,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
   }
 
   const AddName = (event) => {
@@ -33,15 +40,20 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     }
     setNewName('')
+    setNewNumber('')
   }
 
-  const displayPeople = () => persons.map(person => 
+  const displayPeople = () => persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())).map(person => 
     <Person name={person.name} key={person.name} number={person.number} />
   )
+  
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form></form>
+      <div>filter shown with <input value={filter} onChange={handleFilterChange} /></div>
+      <h2>add a new</h2>
       <form onSubmit={AddName}>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
